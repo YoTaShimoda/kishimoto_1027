7dd4a26218d55fdd46ac12a6577b025e5b21cdd2
下田陽太 <shimoda@weseek.co.jp>
2019-11-02 23:27:04 +0900

    必要ないファイル削除

--- csv_methods.py
+++
@@ -1,6 +0,0 @@
-def read_data(file_name):
-    with open(file_name, encoding='utf-8') as f:
-        reader = csv.reader(f)
-        list = [row for row in reader]
-        return list
-def total_heart_rate_count(target_string, )
