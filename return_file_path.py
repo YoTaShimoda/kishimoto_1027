cd4240da6bf1d84968b708191765cb5106c99ed9
下田陽太 <shimoda@weseek.co.jp>
2019-11-10 10:55:22 +0900

    ファイルパスを相対パスに変更

--- return_file_path.py
+++ return_file_path.py
@@ -1,11 +1,11 @@
 import glob
 
 def numfour_return_file_path():
-    numfour = glob.glob('/Users/yota/kishimoto_1027/result/result4/*')
+    numfour = glob.glob('result/result4/*')
     numfour = sorted(numfour)
     return numfour
 
 def numfive_return_file_path():
-    numfive = glob.glob('/Users/yota/kishimoto_1027/result/result5/*')
+    numfive = glob.glob('result/result5/*')
     numfive = sorted(numfive)
     return numfive
